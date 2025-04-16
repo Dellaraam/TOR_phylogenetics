a1ec84c5d53956fcafc62c038dc8e109cac4c116
Dellaraam <136400422+Dellaraam@users.noreply.github.com>
2025-04-16 14:26:42 -0700

    removerictor

--- Phylogenetics/HTML_Additions.R
+++ Phylogenetics/HTML_Additions.R
@@ -40,7 +40,6 @@ HTML <- HTML %>%
   proteinPossible("Effrenium voratum","LST8","P") %>%
   proteinPossible("Paramecium primaurelia","RICTOR","P")%>%
   proteinPossible("Bonamia ostreae","RAPTOR","P")%>%
-  proteinPossible("Lotharella oceanica","RICTOR","P")%>%
   proteinPossible("Lotharella oceanica","RAPTOR","P")%>%
   proteinPossible("Lotharella oceanica","TOR","P")%>%
   proteinPossible("Lotharella oceanica","LST8","P")%>%
