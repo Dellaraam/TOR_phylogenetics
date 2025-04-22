17b0f1311ef67fa80aa683cc152cf09d85750b5b
Kyle <39416934+kajohnson6614@users.noreply.github.com>
2025-04-22 10:11:03 -0700

    Update Raw_Data_Graphs.R

--- Phylogenetics/Raw_Data_Graphs.R
+++ Phylogenetics/Raw_Data_Graphs.R
@@ -34,6 +34,12 @@ StramRictorJGI$Accn<- sub("\\_.*", "", StramRictorJGI$Accn)
 StramRictorJGI <- merge(StramRictorJGI,PhycoInformation[c("Organism_Taxonomic_ID", "name", "Accn")], by="Accn")
 StramRictorJGI <- rename(StramRictorJGI, Organism.Name = "name")
 
+StramRaptorJGI <- read.csv("C:/Users/kajoh/Documents/GitHub/TOR_phylogenetics/GitHub_CSV/Heterokonta_Stramenopiles/Heterokonts_RAPTOR.csv")
+StramRaptorJGI$Accn<- sub("\\_.*", "", StramRaptorJGI$Accn)
+StramRaptorJGI <- merge(StramRaptorJGI,PhycoInformation[c("Organism_Taxonomic_ID", "name", "Accn")], by="Accn")
+StramRaptorJGI <- rename(StramRaptorJGI, Organism.Name = "name")
+
+
 
 StramRictorNCBI <- read.csv(file="C:/Users/kajoh/Documents/GitHub/TOR_phylogenetics/GitHub_CSV/Updated_Stramenopiles/RICTORStramenopiles.csv")
 StramRictorNCBI <- merge(StramRictorNCBI, StramInformationNCBI[c("Organism_Taxonomic_ID","Accn", "Organism.Name")], by = "Accn")
@@ -45,7 +51,14 @@ FinalStramRictor %>% ggplot(aes(x = sca, y = scd, color = Group.name))+
   xlab(label = "Overall HMMER Score")+
   ylab(label = "Best Domain Hit Score")+
   labs(color = "Group")
-  
+
+
+StramRaptorNCBI <- read.csv(file = "C:/Users/kajoh/Documents/GitHub/TOR_phylogenetics/GitHub_CSV/Updated_Stramenopiles/RAPTORStramenopiles.csv")
+StramRaptorNCBI <- merge(StramRaptorNCBI, StramInformationNCBI[c("Organism_Taxonomic_ID","Accn", "Organism.Name")], by = "Accn")
+FinalStramRaptor <- rbind(StramRaptorNCBI,StramRaptorJGI)
+FinalStramRaptor <- merge(FinalStramRaptor, TaxonomicInformation, by = "Organism_Taxonomic_ID")
+FinalStramRaptor %>% ggplot(aes(x = Group.name, y = sca))+
+  geom_jitter()
 
 
 
@@ -61,6 +74,15 @@ ChloroRictorJGI$Accn<- sub("\\_.*", "", ChloroRictorJGI$Accn)
 ChloroRictorJGI <- merge(ChloroRictorJGI,PhycoInformation[c("Organism_Taxonomic_ID", "name", "Accn")], by="Accn")
 ChloroRictorJGI <- rename(ChloroRictorJGI, Organism.Name = "name")
 
+ChloroRaptorJGI <- read.csv(file = "C:/Users/kajoh/Documents/GitHub/TOR_phylogenetics/GitHub_CSV/Chlorophyta_JGI/ChlorophytaJGI_RAPTOR.csv")
+ChloroRaptorJGI$Accn<- sub("\\_.*", "", ChloroRaptorJGI$Accn)
+ChloroRaptorJGI <- merge(ChloroRaptorJGI,PhycoInformation[c("Organism_Taxonomic_ID", "name", "Accn")], by="Accn")
+ChloroRaptorJGI <- rename(ChloroRaptorJGI, Organism.Name = "name")
+
+
+
+
+
 ChloroRictorNCBI <- read.csv(file="C:/Users/kajoh/Documents/GitHub/TOR_phylogenetics/GitHub_CSV/Updated_Chlorophyta/RICTOR_Chlorophyta.csv")
 ChloroRictorNCBI <- merge(ChloroRictorNCBI, ChloroInformationNCBI[c("Organism_Taxonomic_ID","Accn", "Organism.Name")], by = "Accn")
 FinalChloroRictor <- rbind(ChloroRictorNCBI, ChloroRictorJGI)
@@ -71,6 +93,25 @@ FinalChloroRictor %>% ggplot(aes(x = Group.name, y = sca))+
   xlab(label = "Group")+
   ylab(label = "Overall HMMER Score")+
   labs(color = "Best Domain Hit")
+
+
+ChloroRaptorNCBI <- read.csv(file="C:/Users/kajoh/Documents/GitHub/TOR_phylogenetics/GitHub_CSV/Updated_Chlorophyta/RAPTOR_Chlorophyta.csv")
+ChloroRaptorNCBI <- merge(ChloroRaptorNCBI, ChloroInformationNCBI[c("Organism_Taxonomic_ID","Accn", "Organism.Name")], by = "Accn")
+FinalChloroRaptor <- rbind(ChloroRaptorNCBI, ChloroRaptorJGI)
+FinalChloroRaptor <- merge(FinalChloroRaptor, TaxonomicInformation, by = "Organism_Taxonomic_ID")
+FinalChloroRaptor %>% ggplot(aes(x = Group.name, y = sca))+
+  geom_jitter()+
+  theme_minimal()
+
+
+
+
+
+
+
+
+
+
 
 
 
